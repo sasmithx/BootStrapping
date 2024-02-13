5fdbd3b57fe90b7e106150f5d39977f01a7d64f6
sasa-x <sasmithgit@gmail.com>
2024-02-13 18:52:02 +0530

    SessionFactoryConfig implement

--- src/main/java/lk/ijse/nativeBootStrapping/config/SessionFactoryConfig.java
+++ src/main/java/lk/ijse/nativeBootStrapping/config/SessionFactoryConfig.java
@@ -1,4 +1,23 @@
 package lk.ijse.nativeBootStrapping.config;
 
+import com.mysql.cj.Session;
+
+import java.util.Properties;
+
 public class SessionFactoryConfig {
+    private static SessionFactoryConfig factoryConfig;
+
+    private SessionFactoryConfig() {}
+
+    public static SessionFactoryConfig getInstance(){
+        return (factoryConfig == null)?
+                factoryConfig=new SessionFactoryConfig():factoryConfig;
+    }
+
+    public Session getSession(){
+        Properties properties = new Properties();
+
+        properties.load(ClassLoader.getSystemClassLoader()).get;
+    }
+
 }
