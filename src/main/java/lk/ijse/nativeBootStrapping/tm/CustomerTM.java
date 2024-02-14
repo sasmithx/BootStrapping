904a6f0c3bd2341eccef96a58c1a9abe1c2ab438
sasa-x <sasmithgit@gmail.com>
2024-02-14 13:18:42 +0530

    TM updated

--- src/main/java/lk/ijse/nativeBootStrapping/tm/CustomerTM.java
+++ src/main/java/lk/ijse/nativeBootStrapping/tm/CustomerTM.java
@@ -1,4 +1,15 @@
 package lk.ijse.nativeBootStrapping.tm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CustomerTM {
+    private String id;
+    private String name;
+    private String address;
+    private int mobile_num;
 }
--- src/main/java/lk/ijse/nativeBootStrapping/tm/ItemTM.java
+++ src/main/java/lk/ijse/nativeBootStrapping/tm/ItemTM.java
@@ -1,4 +1,15 @@
 package lk.ijse.nativeBootStrapping.tm;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ItemTM {
+    private String id;
+    private String name;
+    private int qty;
+    private double price;
 }
